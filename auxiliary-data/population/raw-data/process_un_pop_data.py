a7e5f156337aea073ae1a193e714f63c480baf0e
ngozzi <nic.gozzi@gmail.com>
2024-06-07 11:40:28 +0200

    fixed name in pop data

--- auxiliary-data/population/raw-data/process_un_pop_data.py
+++ auxiliary-data/population/raw-data/process_un_pop_data.py
@@ -5,6 +5,7 @@ def get_country_population(df_total, iso2, year, mult_factor=1000):
     df_country = df_total.loc[(df_total.Time == year) & \
                               (df_total.ISO2_code == iso2)][["AgeGrp", "PopTotal"]].reset_index(drop=True)
     df_country["PopTotal"] = (df_country.PopTotal * mult_factor).astype(int)
+    df_country.rename(columns={"AgeGrp": "age_group", "PopTotal": "population"}, inplace=True)
     return df_country
 
 # import population data from https://population.un.org/wpp/Download/Standard/CSV/
